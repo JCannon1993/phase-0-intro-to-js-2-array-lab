aab1dab215e4a5edd204357d39da0a7562985102
Jelani Cannon <jelani.cannon@gmail.com>
2022-09-23 16:38:31 -0700

    commit tree fa11df27568e04bf72c9eb6a3dcacbdee8e1feda

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+ function destructivelyRemoveLastCat(){
+     cats.pop()
+ }
+
+ function destructivelyRemoveFirstCat(){
+     cats.shift()
+ }
+
+ function appendCat(name){
+     const newCats = [...cats , name]
+     return newCats
+ }
+
+ function prependCat(name){
+     const addCats = [name, ...cats]
+     return addCats
+ }
+
+ function removeLastCat(){
+     const blow = cats.slice(0,2)
+     return blow
+ }
+
+ function removeFirstCat(){
+     const bab = cats.slice(1)
+     return bab
+ }
